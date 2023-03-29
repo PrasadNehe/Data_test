4f7da7529b00be70f413b998064c14f2f4b888ec
PrasadNehe <78457395+PrasadNehe@users.noreply.github.com>
2023-03-29 13:49:31 +0530

    code added

---
+++ Untitled10.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+
+
+
+# In[3]:
+
+
+
+
+
+# In[4]:
+
+
+df=pd.read_csv('Sales.csv')
+
+
+# In[5]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
